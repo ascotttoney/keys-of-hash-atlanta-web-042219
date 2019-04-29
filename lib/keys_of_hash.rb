1c871c78b8de32e22cb28543369e21eb74974615
ascotttoney <ascotttoney@gmail.com>
2019-04-29 17:42:11 -0400

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,22 @@
+require 'pry'
+
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*args)
+    # array = []
+    
+    # arguments.map do |key, value|
+    #   if arguments.include?(value)
+    #       array << key
+    #   end
+    # end
+
+    # binding.pry
+
+    # array
+
+    map do |key, value|
+      args.include?(value) ? key : nil
+    end.compact
+
   end
 end
